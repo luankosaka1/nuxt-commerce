358233220fb5a8edd5627732947ba58faeb0b520
lkosaka <lkosaka@sonco.com>
2025-02-04 11:40:51 -0300

    feat: create newsletter area

---
+++ components/v1/Newsletter.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+async function handleFormSubmit(event: any) {
+  event.preventDefault();
+
+  const email = event.target['newsletter-email'].value;
+
+  try {
+    const response = await GqlSubscribeEmailToNewsletter(email);
+  } catch (error) {
+    console.error(error);
+  }
+}
+</script>
+
+<template>
+  <div class="newsletter v1">
+    <div class="columns">
+      <div class="column-left">
+        <NuxtImg src="https://picsum.photos/1980/450?random=1" alt="Newsletter" />
+      </div>
+      <div class="column-right">
+        <h2>Newsletter</h2>
+        <p>Subscribe to our newsletter to get the latest news and updates.</p>
+        <form class="form" @submit="handleFormSubmit">
+          <div class="form-label">
+            <input type="email" name="newsletter-email" required placeholder="Enter your email address" />
+          </div>
+          <button class="btn btn-primary" type="submit">Subscribe</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="page page-index">
-      <V1Caroucel />
-      <V1Beneficies />
-      <V1TopProducts />
-    </div>
+  <div class="page page-index">
+    <V1Caroucel />
+    <V1Beneficies />
+    <V1TopProducts />
+    <V1Newsletter />
+  </div>
 </template>
 <style lang="scss">
-  .page {
-    &.page-index {
-      display: flex;
-      flex-direction: column;
-      gap: 3.2rem;
-    }
+.page {
+  &.page-index {
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
   }
+}
 </style>
